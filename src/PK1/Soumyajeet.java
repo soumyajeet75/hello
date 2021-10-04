eaa6ec9c5b65d05296c17c5a7fb32da8bd6bcfc2
soumyajeet75 <67822031+soumyajeet75@users.noreply.github.com>
2021-10-05 04:10:11 +0530

    COMMIT FROM TEAM MEMBER SOUMYAJEET

--- src/PK1/Soumyajeet.java
+++ src/PK1/Soumyajeet.java
@@ -5,6 +5,7 @@ public class Soumyajeet {
 	public static void main(String[] args) {
 		System.out.println("HELLO SOUMYAJEET");
 		System.out.println("I AM AN SOFTWARE ENGINEER AT APISERo)");
+		System.out.println("sum of two numbers AND SOUMYA ");
 
 	}
 
